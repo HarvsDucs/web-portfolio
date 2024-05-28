4f60b66d54010c66de22cd5eb746ffa8eae02249
Harvey Ducay <ducs1007@gmail.com>
2024-05-28 12:19:39 +0800

    added photos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,11 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import image1 from '@/images/logos/bitcoin.png'
+import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/portrait.jpeg'
+import image5 from '@/images/photos/image-5.jpg'
 
 
 
